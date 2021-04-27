b2ae9b5d856d1c64742e256df4e98ba547e34803
mkhoirurrizqi <muhammad.118140134@student.itera.ac.id>
2021-04-27 21:52:46 +0700

    minor style update

--- components/all/EditProfile.jsx
+++ components/all/EditProfile.jsx
@@ -1,41 +1,39 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity, Image, StatusBar, ToastAndroid } from "react-native";
 
-const EditProfile  = (props) => {
+const EditProfile = (props) => {
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
   const [whatsapp, setWhatsapp] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirm_password, setConfirm_pasword] = useState("");
-    return (
-      <View style={{ flex: 1, alignItems: 'center' ,backgroundColor:"white"}}>
-      <ScrollView >
-      
+  return (
+    <View style={{ flex: 1, alignItems: "center", backgroundColor: "white" }}>
+      <ScrollView>
         <Text style={styles.header}>Edit Profile</Text>
         <View style={styles.textInputWrapper}>
-        <TextInput style={styles.textInput} onChangeText={(name) => setName( name )} value={name} placeholder="Name" placeholderTextColor="grey" />
-          <TextInput style={styles.textInput} onChangeText={(username) => setUsername( username )} value={username} placeholder="Username" placeholderTextColor="grey" />
-          <TextInput keyboardType="numeric" style={styles.textInput} onChangeText={(whatsapp) => setWhatsapp( whatsapp )} value={whatsapp} placeholder="Whatsapp" placeholderTextColor="grey" />
-          <TextInput style={styles.textInput} onChangeText={(Email) => setEmail( Email )} value={email} placeholder="Email" placeholderTextColor="grey" />
-          <TextInput secureTextEntry={true} style={styles.textInput} onChangeText={(password) => setPassword( password )} value={password} placeholder="Password" placeholderTextColor="grey" />
-          <TextInput secureTextEntry={true} style={styles.textInput} onChangeText={(Confirm_password) => setConfirm_pasword( Confirm_password )} value={confirm_password} placeholder="Confirmation Password" placeholderTextColor="grey" />
-         </View>
+          <TextInput style={styles.textInput} onChangeText={(name) => setName(name)} value={name} placeholder="Name" placeholderTextColor="grey" />
+          <TextInput style={styles.textInput} onChangeText={(username) => setUsername(username)} value={username} placeholder="Username" placeholderTextColor="grey" />
+          <TextInput keyboardType="numeric" style={styles.textInput} onChangeText={(whatsapp) => setWhatsapp(whatsapp)} value={whatsapp} placeholder="Whatsapp" placeholderTextColor="grey" />
+          <TextInput style={styles.textInput} onChangeText={(Email) => setEmail(Email)} value={email} placeholder="Email" placeholderTextColor="grey" />
+          <TextInput secureTextEntry={true} style={styles.textInput} onChangeText={(password) => setPassword(password)} value={password} placeholder="Password" placeholderTextColor="grey" />
+          <TextInput secureTextEntry={true} style={styles.textInput} onChangeText={(Confirm_password) => setConfirm_pasword(Confirm_password)} value={confirm_password} placeholder="Confirmation Password" placeholderTextColor="grey" />
+        </View>
         <View style={styles.buttonWrapper}>
-          <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate('Profile')}>
+          <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("Profile")}>
             <Text style={styles.buttonText}>Save</Text>
           </TouchableOpacity>
         </View>
-    
       </ScrollView>
-      </View>
-    );
-  }
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-dropdownWrapper:{
+  dropdownWrapper: {
     width: 200,
     alignSelf: "flex-start",
-},
+  },
   header: {
     fontSize: 40,
     fontWeight: "bold",
--- components/store/EditProduct.jsx
+++ components/store/EditProduct.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity, Image, StatusBar, ToastAndroid } from "react-native";
-import DropDownPicker from 'react-native-dropdown-picker';
+import DropDownPicker from "react-native-dropdown-picker";
 
 class EditProduct extends React.Component {
   constructor(props) {
@@ -19,53 +19,62 @@ class EditProduct extends React.Component {
   }
   render() {
     return (
-      
-      <ScrollView >
-        <View style={{ flex: 1, alignItems: 'center' ,backgroundColor:"white"}}>
-        <View style={styles.textInputWrapper}>
-          <TextInput style={styles.textInput} onChangeText={(productname) => this.setState({ productname })} value={this.state.productname} placeholder="Product Name" placeholderTextColor="grey" />
-          <TextInput keyboardType="numeric" style={styles.textInput} onChangeText={(price) => this.setState({ price })} value={this.state.price} placeholder="Price" placeholderTextColor="grey" />
-          <TextInput style={styles.textInput} onChangeText={(weight) => this.setState({ weight })} value={this.state.weight} placeholder="Weight" placeholderTextColor="grey" />
-          <TextInput style={styles.textInput} onChangeText={(jenis) => this.setState({ jenis })} value={this.state.jenis} placeholder="Jenis" placeholderTextColor="grey" />
-          <TextInput style={styles.textInput} onChangeText={(expired) => this.setState({ expired })} value={this.state.expired} placeholder="Expired" placeholderTextColor="grey" />
-          <TextInput style={styles.textareaInput} multiline={true} numberOfLines={6} onChangeText={(deskripsi) => this.setState({ deskripsi })} value={this.state.deskripsi} placeholder="Type the description here ..." placeholderTextColor="grey" />
-          <TextInput style={styles.textInput} onChangeText={(photo) => this.setState({ photo })} value={this.state.photo} placeholder="Choose File " placeholderTextColor="grey" />
-        
-        <View style={styles.dropdownWrapper}>
-        <DropDownPicker 
-            placeholder="Status"
-            items={[
-                {label: 'Ready Stock', value: 'Ready Stock'},
-                {label: 'Out Of Stock', value: 'Out Of Stock'},
-             ]}
-            containerStyle={{height: 40}}
-            style={{backgroundColor: "white"}}
-            itemStyle={{
-                 justifyContent: "flex-start"
-            }}
-            dropDownStyle={{backgroundColor: "white"}}
-            onChangeItem={item => this.setState({
-                role: item.value
-            })}
+      <ScrollView style={{ backgroundColor: "white" }}>
+        <View style={{ flex: 1, alignItems: "center", paddingBottom: 50 }}>
+          <View style={styles.textInputWrapper}>
+            <TextInput style={styles.textInput} onChangeText={(productname) => this.setState({ productname })} value={this.state.productname} placeholder="Product Name" placeholderTextColor="grey" />
+            <TextInput keyboardType="numeric" style={styles.textInput} onChangeText={(price) => this.setState({ price })} value={this.state.price} placeholder="Price" placeholderTextColor="grey" />
+            <TextInput style={styles.textInput} onChangeText={(weight) => this.setState({ weight })} value={this.state.weight} placeholder="Weight" placeholderTextColor="grey" />
+            <TextInput style={styles.textInput} onChangeText={(jenis) => this.setState({ jenis })} value={this.state.jenis} placeholder="Jenis" placeholderTextColor="grey" />
+            <TextInput style={styles.textInput} onChangeText={(expired) => this.setState({ expired })} value={this.state.expired} placeholder="Expired" placeholderTextColor="grey" />
+            <TextInput
+              style={styles.textareaInput}
+              multiline={true}
+              numberOfLines={6}
+              onChangeText={(deskripsi) => this.setState({ deskripsi })}
+              value={this.state.deskripsi}
+              placeholder="Type the description here ..."
+              placeholderTextColor="grey"
             />
+            <TextInput style={styles.textInput} onChangeText={(photo) => this.setState({ photo })} value={this.state.photo} placeholder="Choose File " placeholderTextColor="grey" />
+
+            <View style={styles.dropdownWrapper}>
+              <DropDownPicker
+                placeholder="Status"
+                items={[
+                  { label: "Ready Stock", value: "Ready Stock" },
+                  { label: "Out Of Stock", value: "Out Of Stock" },
+                ]}
+                containerStyle={{ height: 40 }}
+                style={{ backgroundColor: "white" }}
+                itemStyle={{
+                  justifyContent: "flex-start",
+                }}
+                dropDownStyle={{ backgroundColor: "white" }}
+                onChangeItem={(item) =>
+                  this.setState({
+                    role: item.value,
+                  })
+                }
+              />
             </View>
-            </View>
-        <View style={styles.buttonWrapper}>
-          <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('StoreProduct')}>
-            <Text style={styles.buttonText}>Save</Text>
-          </TouchableOpacity>
+          </View>
+          <View style={styles.buttonWrapper}>
+            <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate("StoreProduct")}>
+              <Text style={styles.buttonText}>Save</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
       </ScrollView>
     );
   }
 }
 const styles = StyleSheet.create({
-dropdownWrapper:{
+  dropdownWrapper: {
     width: 200,
     alignSelf: "flex-start",
-    marginTop:20,
-},
+    marginTop: 20,
+  },
 
   textInputWrapper: {
     marginBottom: 10,
@@ -80,10 +89,10 @@ dropdownWrapper:{
   textareaInput: {
     color: "#000000",
     marginTop: 30,
-    borderWidth:1,
-    paddingLeft:5,
-    paddingTop:10,
-    textAlignVertical:"top"
+    borderWidth: 1,
+    paddingLeft: 5,
+    paddingTop: 10,
+    textAlignVertical: "top",
   },
   buttonWrapper: {
     width: 350,
