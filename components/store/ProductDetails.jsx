8ae9d37ebd1532a40d47dafbe0b5f5b409f52147
mkhoirurrizqi <muhammad.118140134@student.itera.ac.id>
2021-05-10 18:47:35 +0700

    connect product list page to api

--- components/store/ProductDetails.jsx
+++ components/store/ProductDetails.jsx
@@ -1,162 +1,95 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity, Image, StatusBar, ToastAndroid } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useSelector } from "react-redux";
 
 const StoreProduct = (props) => {
-  const [productName, setProductName] = useState("");
+  const token = useSelector((data) => data.user.token);
+  const [productName, setProductName] = useState([]);
+  const [productArray, setProductArray] = useState([]);
+  const id = useSelector((data) => data.user.id);
+  useEffect(() => {
+    fetch("https://pamparampam.herokuapp.com/api/storeproduct", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id: id,
+      }),
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then((responseJson) => {
+        setProductName([]);
+        // if (productName.length <= responseJson.length) {
+        responseJson.forEach((element) => {
+          setProductName((productName) => [...productName, element.pd_name]);
+        });
+        // // }
+        // console.log(productName);
+        console.log(responseJson);
+        console.log(id);
+        console.log(productName);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  // if (id) {
+  //   fetch("https://pamparampam.herokuapp.com/api/storeproduct", {
+  //     method: "POST",
+  //     headers: {
+  //       Accept: "application/json",
+  //       Authorization: "Bearer " + token,
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       user_id: id,
+  //     }),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((responseJson) => {})
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }
+
   return (
     <ScrollView style={{ backgroundColor: "white" }}>
       <View style={styles.container}>
         <View style={styles.allProduct}>
-          <View style={styles.product}>
-            <View style={styles.button2}>
-              <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("AddProduct")}>
-                <Text style={styles.buttonText}>Add</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.content}>
-              <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
-                <MaterialCommunityIcons name="cake" size={30} color={"#F57373"} />
-              </View>
-              <View style={{ flex: 7, justifyContent: "center" }}>
-                <Text style={styles.CakeName}>Kaastengels </Text>
-              </View>
-              <View style={{ flex: 7, alignItems: "center", justifyContent: "center", flexDirection: "row" }}>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("EditProduct")}>
-                  <Text style={styles.buttonText}>Edit</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("")}>
-                  <Text style={styles.buttonText}>Delete</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
+          <View style={styles.button2}>
+            <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("AddProduct")}>
+              <Text style={styles.buttonText}>Add</Text>
+            </TouchableOpacity>
           </View>
-          <View style={styles.product}>
-            <View style={styles.content}>
-              <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
-                <MaterialCommunityIcons name="cake" size={30} color={"#F57373"} />
+          {productName.map((product) => {
+            return (
+              <View style={styles.product}>
+                <View style={styles.content}>
+                  <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
+                    <MaterialCommunityIcons name="cake" size={30} color={"#F57373"} />
+                  </View>
+                  <View style={{ flex: 7, justifyContent: "center" }}>
+                    <Text style={styles.CakeName}>{product}</Text>
+                  </View>
+                  <View style={{ flex: 7, alignItems: "center", justifyContent: "center", flexDirection: "row" }}>
+                    <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("EditProduct")}>
+                      <Text style={styles.buttonText}>Edit</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("")}>
+                      <Text style={styles.buttonText}>Delete</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
               </View>
-              <View style={{ flex: 7, justifyContent: "center" }}>
-                <Text style={styles.CakeName}>Putri Salju</Text>
-              </View>
-              <View style={{ flex: 7, alignItems: "center", justifyContent: "center", flexDirection: "row" }}>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("EditProduct")}>
-                  <Text style={styles.buttonText}>Edit</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("")}>
-                  <Text style={styles.buttonText}>Delete</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
-          <View style={styles.product}>
-            <View style={styles.content}>
-              <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
-                <MaterialCommunityIcons name="cake" size={30} color={"#F57373"} />
-              </View>
-              <View style={{ flex: 7, justifyContent: "center" }}>
-                <Text style={styles.CakeName}>Nastar</Text>
-              </View>
-              <View style={{ flex: 7, alignItems: "center", justifyContent: "center", flexDirection: "row" }}>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("EditProduct")}>
-                  <Text style={styles.buttonText}>Edit</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("")}>
-                  <Text style={styles.buttonText}>Delete</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
-          <View style={styles.product}>
-            <View style={styles.content}>
-              <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
-                <MaterialCommunityIcons name="cake" size={30} color={"#F57373"} />
-              </View>
-              <View style={{ flex: 7, justifyContent: "center" }}>
-                <Text style={styles.CakeName}>Choco Chips &ensp;</Text>
-              </View>
-              <View style={{ flex: 7, alignItems: "center", justifyContent: "center", flexDirection: "row" }}>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("EditProduct")}>
-                  <Text style={styles.buttonText}>Edit</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("")}>
-                  <Text style={styles.buttonText}>Delete</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
-          <View style={styles.product}>
-            <View style={styles.content}>
-              <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
-                <MaterialCommunityIcons name="cake" size={30} color={"#F57373"} />
-              </View>
-              <View style={{ flex: 7, justifyContent: "center" }}>
-                <Text style={styles.CakeName}>Wafer Keju</Text>
-              </View>
-              <View style={{ flex: 7, alignItems: "center", justifyContent: "center", flexDirection: "row" }}>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("EditProduct")}>
-                  <Text style={styles.buttonText}>Edit</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("")}>
-                  <Text style={styles.buttonText}>Delete</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
-          <View style={styles.product}>
-            <View style={styles.content}>
-              <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
-                <MaterialCommunityIcons name="cake" size={30} color={"#F57373"} />
-              </View>
-              <View style={{ flex: 7, justifyContent: "center" }}>
-                <Text style={styles.CakeName}>Sagu Keju</Text>
-              </View>
-              <View style={{ flex: 7, alignItems: "center", justifyContent: "center", flexDirection: "row" }}>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("EditProduct")}>
-                  <Text style={styles.buttonText}>Edit</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("")}>
-                  <Text style={styles.buttonText}>Delete</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
-          <View style={styles.product}>
-            <View style={styles.content}>
-              <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
-                <MaterialCommunityIcons name="cake" size={30} color={"#F57373"} />
-              </View>
-              <View style={{ flex: 7, justifyContent: "center" }}>
-                <Text style={styles.CakeName1}>Legit</Text>
-              </View>
-              <View style={{ flex: 7, alignItems: "center", justifyContent: "center", flexDirection: "row" }}>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("EditProduct")}>
-                  <Text style={styles.buttonText}>Edit</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("")}>
-                  <Text style={styles.buttonText}>Delete</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
-          <View style={styles.product}>
-            <View style={styles.content}>
-              <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
-                <MaterialCommunityIcons name="cake" size={30} color={"#F57373"} />
-              </View>
-              <View style={{ flex: 7, justifyContent: "center" }}>
-                <Text style={styles.CakeName1}>Engkak &nbsp; &nbsp; </Text>
-              </View>
-              <View style={{ flex: 7, alignItems: "center", justifyContent: "center", flexDirection: "row" }}>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("EditProduct")}>
-                  <Text style={styles.buttonText}>Edit</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("")}>
-                  <Text style={styles.buttonText}>Delete</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
+            );
+          })}
         </View>
       </View>
     </ScrollView>
